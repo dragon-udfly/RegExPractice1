6dc09fa3ee54c520d7814a70d88db2c61a7d32f9
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-18 22:00:04 +0530

    Updated the text

--- script2.py
+++ script2.py
@@ -1,6 +1,6 @@
 import re
 
-text= "This is a 399 and 89 but but ."
+text= "This is a 399 and 89 but-    but ."
 print(f"Original text: {text}")
 digits= re.findall("\d", text)
 print(f"Digits: {digits}")
