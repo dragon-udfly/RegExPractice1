9e900d0d24a7b13c21ef21d7a6fbb35000998193
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-18 23:44:16 +0530

    Created a function to print characters in the list

--- script3.py
+++ script3.py
@@ -1,9 +1,12 @@
 import re 
 
+def Print(arr):
+    for ch in range(0, len(arr), 1):
+        print(arr[ch])
+    print("\n")
+
 text= str(input("Enter text here: "))
 
 print(f"Original Text: {text}")
 
 a_zCharacters= re.findall("[a-z]", text)
-for ch in range(0, len(a_zCharacters), 1):
-    print(a_zCharacters[ch], end=' ')
