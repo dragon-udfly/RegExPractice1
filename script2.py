aebeff103e7af079c291e14e202fafdfa02387f8
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-18 21:57:23 +0530

    Used to find all non digital characters in the text

--- script2.py
+++ script2.py
@@ -4,3 +4,5 @@ text= "This is a 399 and 89 but but ."
 print(f"Original text: {text}")
 digits= re.findall("\d", text)
 print(f"Digits: {digits}")
+nondigits= re.findall("\D", text)
+print(f"Non Digits: {nondigits}")
