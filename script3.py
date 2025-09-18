497786c4896d07ca9c14e355b86150e41e7fef72
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-18 23:39:33 +0530

    Updated the code

--- script3.py
+++ script3.py
@@ -5,4 +5,5 @@ text= str(input("Enter text here: "))
 print(f"Original Text: {text}")
 
 a_zCharacters= re.findall("[a-z]", text)
-print("Alphabetical Characters: {a_zCharacters}")
+for ch in range(0, len(a_zCharacters), 1):
+    print(a_zCharacters[ch], end=' ')
