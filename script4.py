16a942f4adabb0aa5e27124a583f147d378e703e
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-19 20:16:49 +0530

    Used to find all english characters between a to z

--- script4.py
+++ script4.py
@@ -1,1 +1,6 @@
 import re 
+
+text= "This text is containing, is running, is walking. Not the same thing but the only thing. Can you be running."
+
+english_characters= re.findall("[a-z]", text)
+print(f"Number of english characters: {len(english_characters)}")
