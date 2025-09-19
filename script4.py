28fa0d6022339dd54a9e92611f2096995d7555d6
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-19 20:24:06 +0530

    Used to find letters and symbols in the text

--- script4.py
+++ script4.py
@@ -13,3 +13,6 @@ print(f"Number of non digital characters: {len(non_digits)}")
 
 non_word_characters= re.findall("\W", text) 
 print(f"Number of non-word characters: {len(non_word_characters)}")
+
+letters_and_numbers= re.findall("\w", text) 
+print(f"Number of letters and characters: {len(letters_and_numbers)}")
