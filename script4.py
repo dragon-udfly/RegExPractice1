c67d832631c3df01aa60269af0f4492a801200cf
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-19 20:17:23 +0530

    Updated the text

--- script4.py
+++ script4.py
@@ -1,6 +1,7 @@
 import re 
 
-text= "This text is containing, is running, is walking. Not the same thing but the only thing. Can you be running."
+text= "This text is containing89, is running34, is walking 45. Not the same thing but the only thing. 23 Can you be running."
 
 english_characters= re.findall("[a-z]", text)
 print(f"Number of english characters: {len(english_characters)}")
+
