9b9e968ab91781915453f321d1ed47615e4f7cdc
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-19 20:55:42 +0530

    Used to count all literal periods in the text

--- script5.py
+++ script5.py
@@ -2,3 +2,4 @@ import re
 
 literal_periods= re.findall("\.", "This . not the. way you think.")
 print(f"Literal periods: {literal_periods}")
+print(f"Number of literal periods: {len(literal_periods)}")
