64352b648c97564ad1744c093bedd83650949a9a
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-09-19 20:57:48 +0530

    Used to find all literal astrisk and count

--- script5.py
+++ script5.py
@@ -3,3 +3,7 @@ import re
 literal_periods= re.findall("\.", "This . not the. way you think.")
 print(f"Literal periods: {literal_periods}")
 print(f"Number of literal periods: {len(literal_periods)}")
+
+literal_astrisk= re.findall("\*", "This * astrisk as it is *.")
+print(f"Literal astrisks: {literal_astrisk}")
+print(f"Number of literal astrisks: {len(literal_astrisk)}")
